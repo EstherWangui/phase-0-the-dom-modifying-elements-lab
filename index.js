c9a32bb829ecc1892852519155898955bb3fcac1
EstherWangui <esther.nganga@student.moringaschool.com>
2022-08-18 13:30:31 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const element = document.getElementById('main')
+element.remove();
 
+let newHeader = document.createElement('h1')
+newHeader.id = "victory"
+newHeader.innerHTML = "Charles is the champion"
+
+document.body.append(newHeader) 
